9043d178f6bdcaac9fde561a6050feb1b61f9d68
syedyousafraza <syedyousafraza@yahoo.com>
2021-11-10 19:34:37 +0500

    Add files via upload

---
+++ extensionChrome/background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+      "id": "sampleContextMenu",
+      "title": "Sample Context Menu",
+      "contexts": ["selection"]
+    });
+  });
+
+  chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+      "id": "sampleContextMenu",
+      "title": "Sample Context Menu",
+      "contexts": ["selection"]
+    });
+  });
