e46f6d5b7d7d4501b4ca16dcccbcd10dbeb62276
syedyousafraza <syedyousafraza@yahoo.com>
2021-11-15 14:42:37 +0500

    Update wallet.js
    WETH Balance update and added some checks in flow

--- wallet.js
+++ wallet.js
@@ -59,12 +59,8 @@ const { test, expect } = require('@playwright/test');
     await extensionPage.waitForSelector('#password');
     await extensionPage.type('#password', password);
     await extensionPage.click("text=UNLOCK");
-    //  const butt = await extensionPage.waitForSelector("section.popover-wrap.whats-new-popup__popover button.fas.fa-times.popover-header__button");
-    //  await butt.click();
-    //   await extensionPage.waitForSelector('#app-content > div > div.app-header.app-header--back-drop > div > div.app-header__account-menu-container > div.app-header__network-component-wrapper > div > span');
-    //  await extensionPage.click('#app-content > div > div.app-header.app-header--back-drop > div > div.app-header__account-menu-container > div.app-header__network-component-wrapper > div > span');
-    //  await extensionPage.click('#app-content > div > div.menu-droppo-container.network-droppo > div > li:nth-child(7) > span', { delay: 1000 });
   }
+
 
   const newtab = await browserContext.newPage();
 
@@ -75,19 +71,12 @@ const { test, expect } = require('@playwright/test');
     console.log(error);
   }
 
-
-  // await newtab.waitForSelector('.header-content > .right-part > .start-part > .primary-btn > .primary-text')
-  // await newtab.click('.header-content > .right-part > .start-part > .primary-btn > .primary-text')
-
-
-  // // await expect(newtab).toHaveURL('https://goerli-test.zed.run');
-  // // await newtab.click('button:has-text("Start")', { delay: 30000 });
-  // await expect(newtab).toHaveURL('https://goerli-test.zed.run/home/start');
   await newtab.waitForSelector('text=Metamask');
   await newtab.click('text=Metamask');
   await extensionPage.reload();
 
-  if (await extensionPage.$('text=Next') !== null) {
+
+  if (await extensionPage.$("text=Next") !== null) {
     await extensionPage.click('text=Next');
     await extensionPage.click('[data-testid="page-container-footer-next"]');
     await extensionPage.click('[data-testid="request-signature__sign"]');
@@ -96,119 +85,48 @@ const { test, expect } = require('@playwright/test');
     await extensionPage.click('[data-testid="request-signature__sign"]');
   }
 
+
   await newtab.click('.header-container > .header > .header-content > .right-part > .balance-part')
+  const prevBalance = await newtab.waitForSelector('#app > div > div.header-container > header > div.sidebar-wrapper.balance-sidebar.custom-scroll.opened > div.sidebar-content > div.balance-box.zed > div.top > div.wallet-price > div.lg-text.bold.green');
+  await newtab.waitForSelector('#app > div > div.header-container > header > div.sidebar-wrapper.balance-sidebar.custom-scroll.opened');
   await newtab.click('text=Deposit');
   await newtab.click('[placeholder="Enter Amount..."]', { delay: 5000 });
   await newtab.fill('[placeholder="Enter Amount..."]', '0.02');
-  await newtab.waitForSelector('.sidebar-content > .dw-content-container > .dw-content > .matic-deposit > .primary-btn')
-  await newtab.click('.sidebar-content > .dw-content-container > .dw-content > .matic-deposit > .primary-btn')
 
+  await newtab.waitForSelector('text=Deposit to WETH Balance');
+  await newtab.click('text=Deposit to WETH Balance');
+
+  // await newtab.click('.sidebar-content > .dw-content-container > .dw-content > .matic-deposit > .primary-btn')
   await newtab.waitForSelector('.ReactModalPortal > .ReactModal__Overlay > .ReactModal__Content > .buttons-row > .primary-btn')
   await newtab.click('.ReactModalPortal > .ReactModal__Overlay > .ReactModal__Content > .buttons-row > .primary-btn')
   //wait for processing deposit 
   await newtab.waitForSelector('#app > div > div.header-container > header > div.sidebar-wrapper.balance-sidebar.custom-scroll.opened > div.sidebar-content > div.dw-content-container > div.dw-content.open.deposit > div.matic-deposit > button');
 
 
-  // await newtab.click('text=Deposit to WETH Balance', { delay: 3000 });
-  // await newtab.click('button:has-text("Confirm")', { delay: 5000 });
-  // await newtab.waitForSelector('#app > div > div.header-container > header > div.sidebar-wrapper.balance-sidebar.custom-scroll.opened > div.sidebar-content > div.dw-content-container > div.primary-text.warning.m-3.text-center');
   await extensionPage.reload();
+  const confirmTransaction = await extensionPage.$$("text='Confirm'");
+  if (confirmTransaction.length > 0) {
+    await extensionPage.click('[data-testid="page-container-footer-next"]');
+  }
+  else {
+    await extensionPage.reload();
+    await extensionPage.click('[data-testid="page-container-footer-next"]');
+  }
 
+  const newBalance = await newtab.waitForSelector('#app > div > div.header-container > header > div.sidebar-wrapper.balance-sidebar.custom-scroll.opened > div.sidebar-content > div.balance-box.zed > div.top > div.wallet-price > div.lg-text.bold.green', { waitUntil: "networkidle0" });
+  if (prevBalance != newBalance) {
+    console.log("WETH balance updated successfully!")
+    console.log("previous Balance=" + prevBalance)
+  }
 
-  // await extensionPage.waitForSelector('#app-content > div > div.main-container-wrapper > div > div.confirm-page-container-content > div.page-container__footer > footer > button.button.btn-primary.page-container__footer-button');
-  //  await extensionPage.click('#app-content > div > div.main-container-wrapper > div > div.confirm-page-container-content > div.page-container__footer > footer > button.button.btn-primary.page-container__footer-button');
-
-  await extensionPage.click('[data-testid="page-container-footer-next"]');
 
   //logout from the application
-  await newtab.waitForSelector('.user-part > .menu-button > .icon-part > .icon-arrow > .icon');
-  await newtab.click('.user-part > .menu-button > .icon-part > .icon-arrow > .icon', { delay: 5000 });
-  await newtab.waitForSelector('div > div:nth-child(4) > .primary-text > .icon-part > span');
-  await newtab.click('div > div:nth-child(4) > .primary-text > .icon-part > span');
-
-
-
-  // await page.newPage.goto('chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#');
-  // await page.waitForNavigation('chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#');
+  // await newtab.waitForSelector('.user-part > .menu-button > .icon-part > .icon-arrow > .icon');
+  // await newtab.click('.user-part > .menu-button > .icon-part > .icon-arrow > .icon', { delay: 5000 });
+  // await newtab.waitForSelector('div > div:nth-child(4) > .primary-text > .icon-part > span');
+  // await newtab.click('div > div:nth-child(4) > .primary-text > .icon-part > span');
 
   console.log();
 
-
-
-  //  await page.goto('chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#confirm-transaction/', {delay: 30000});
-  // await page.waitUntil('[data-testid="request-signature__sign"]');
-
-  //  page.click('[data-testid="request-signature__sign"]')
-
-
-
-
-
-
-  // await page.goto('chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#confirm-transaction');
-  // page.click('[data-testid="request-signature__sign"]')
-
-
-
-
-
-  //const page1 = await context.newPage();
-  // await page1.goto('chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html');
-  // await page1.goto('chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#connect/dec420c6-e397-4e02-aff4-71a75a615083');
-  // await page1.click('text=Next');
-  // await Promise.all([
-  //   page1.waitForNavigation(/*{ url: 'chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#confirm-transaction/304626331308500/signature-request' }*/),
-  //   page1.click('[data-testid="page-container-footer-next"]')
-  // ]);
-  // Click [data-testid="request-signature__sign"]
-  // await Promise.all([
-  //   page1.waitForNavigation(/*{ url: 'chrome-extension://bjcjcofbnaojipghcbpajdjnipiikeej/notification.html#' }*/),
-  //   page1.click('[data-testid="request-signature__sign"]')
-  // ]);
-  // Close page
-  // await page1.close();
-  // Go to https://goerli-test.zed.run/stable
-  //await page.goto('https://goerli-test.zed.run/stable');
-  // Go to https://goerli-test.zed.run/stable/stable-94224685
-  //await page.goto('https://goerli-test.zed.run/stable/stable-94224685');
-
-
-
-
-
-
-
-
-  // // Get page after a specific action (e.g. clicking a link)
-
-  // await newPage.waitForLoadState();
-  // console.log(await newPage.title());
-  // await page.click('button:has-text("Next")');
-  // await page.click('button:has-text("Connect")');
-
-
-
-
-
-
-  // await extensionPage.click("text=Get Started");
-  // await extensionPage.click("text=Import wallet");
-  // await extensionPage.click("text=I Agree");
-  // await extensionPage.click('#app-content > div > div.main-container-wrapper > div > div > form > div.first-time-flow__textarea-wrapper > div.MuiFormControl-root.MuiTextField-root.first-time-flow__textarea.first-time-flow__seedphrase > div');
-  // await extensionPage.type('#app-content > div > div.main-container-wrapper > div > div > form > div.first-time-flow__textarea-wrapper > div.MuiFormControl-root.MuiTextField-root.first-time-flow__textarea.first-time-flow__seedphrase > div', key);
-  // // await page.type('#password',password);
-  // // await page.type('#confirm-password',password)
-  // await extensionPage.click('#app-content > div > div.main-container-wrapper > div > div > form > div.first-time-flow__checkbox-container > div');
-  // await extensionPage.click('#app-content > div > div.main-container-wrapper > div > div > form > button');
-
-
-  // //  await page.click('#password');
-  // //  await page.type('#password' , password);
-  // await extensionPage.click('#app-content > div > div.main-container-wrapper > div > div > div > button > span');
-  // await extensionPage.click('#app-content > div > div.menu-droppo-container.network-droppo > div > li:nth-child(7) > span');
-
-
-  // await page.click('#app-content > div > div.app-header > div > div.app-header__account-menu-container > div > div')
-
-  await browserContext.close();
+  //  await browserContext.close();
 })();
